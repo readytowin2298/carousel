6a0fd1c9765eb2476128d89afed71f20aff2c522
David Ames <readytowin2298@gmail.com>
2021-05-15 01:19:57 -0500

    fixed left-arrow bug

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,8 +9,16 @@ function Carousel(props) {
   const [cardIdx, setCardIdx] = useState(0);
   const card = props.cardData[cardIdx];
   const total = props.cardData.length;
-  const goForward = () => setCardIdx(cardIdx + 1);
-  const goBackward = () => setCardIdx(cardIdx - 1)
+  const goForward = () => {
+    
+    setCardIdx(cardIdx + 1);
+    
+  }
+  const goBackward = () => {
+    
+    setCardIdx(cardIdx - 1);
+    
+  }
 
   return (
     <div className="Carousel">
@@ -18,7 +26,7 @@ function Carousel(props) {
       <div className="Carousel-main">
         <i
           className="fas fa-chevron-circle-left fa-2x"
-          onClick={goForward}
+          onClick={goBackward}
           data-testid="left-arrow"
         />
         <Card
